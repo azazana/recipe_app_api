56e05b16e053873c45a83a94cf337a59818da505
azazana <azazana@list.ru>
2023-06-06 20:27:10 +0300

    added tests for user

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,53 @@
+"""
+Test for the user API.
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse('user:create')
+
+payload = {
+    "email": "test@example.com",
+    "password": "testpass123",
+    "name": "Test Name",
+}
+
+
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the public features for the user API."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating a user is successful."""
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test_user_eith_email_exists_error(self):
+        """Test error return if user with email exists."""
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL,payload)
+        self.assertEqual(res.stutus_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if passwoed less than 5 chars."""
+        payload["password"]="pw"
+        res = self.client.post(CREATE_USER_URL,payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
+        self.assertFalse(user_exists)
+
