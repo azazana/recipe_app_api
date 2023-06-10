c29f5207e03d56fb9a4954ff60c6108b3e3b12df
azazana <azazana@list.ru>
2023-06-10 16:34:48 +0300

    added authentication

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short_error(self):
         """Test an error is returned if passwoed less than 5 chars."""
-        playload["password"]="pw"
+        payload["password"]="pw"
         res = self.client.post(CREATE_USER_URL,payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
