5e3491ab83281af0666d29e5f0deaf61c4412bc7
azazana <azazana@list.ru>
2023-06-23 15:01:43 +0300

    add test recipes api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,11 +1,12 @@
 """
 Test for models.
 """
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-
 from decimal import Decimal
+
 from core import models
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
 
 class ModelTests(TestCase):
     """Test models."""
@@ -46,6 +47,7 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
@@ -53,11 +55,11 @@ class ModelTests(TestCase):
             "test123pass"
         )
         recipe = models.Recipe.objects.create(
-            user = user,
+            user=user,
             title="Sample recipe name",
-            time_minutes = 5,
-            price = Decimal("5.5"),
-            description = "Sample recipe description",
+            time_minutes=5,
+            price=Decimal("5.5"),
+            description="Sample recipe description",
         )
 
-        self.assertEqual(str(recipe),recipe.title)
+        self.assertEqual(str(recipe), recipe.title)
