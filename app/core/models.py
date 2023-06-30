67b58c80e8360c0c20acf75c7c23abecd6e04d7f
azazana <azazana@list.ru>
2023-06-30 22:27:09 +0300

    serializer tag

--- app/core/models.py
+++ app/core/models.py
@@ -61,12 +61,14 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
+
 class Tag(models.Model):
     """Tag for filtering recipes."""
-    user=models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    name=models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -54,7 +54,7 @@ class PrivateTagsApiTests(TestCase):
     def test_tags_limited_to_user(self):
         """Test list of tags is limited to authenticated user."""
         user2 = create_user(email="user2@example.com")
-        Tag.objects.create(user=user2, name="Fruty")
+        Tag.objects.create(user=user2, name="Fruity")
         tag = Tag.objects.create(user=self.user, name="Comfort food")
         res = self.client.get(TAGS_URL)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
