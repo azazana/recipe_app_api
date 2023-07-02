afc9b319fa4b314c17c84d473cba64eaa6339ffc
azazana <azazana@list.ru>
2023-07-02 17:46:20 +0300

    serializer and tests

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -14,6 +14,11 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse("recipe:ingredient-list")
 
 
+def detail_url(ingredient_id):
+    """Create and return url for the ingredient."""
+    return reverse("recipe:ingredient-detail", args=[ingredient_id])
+
+
 def create_user(email="user@example.com", password="123testpass"):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -62,3 +67,10 @@ class PrivateIngredientsAptTests(TestCase):
         self.assertEqual(res.data[0]["name"], ingredient.name)
         self.assertEqual(res.data[0]["id"], ingredient.id)
 
+    def test_update_ingredient(self):
+        """Test updating ingredient."""
+        ingredient=Ingredient.objects.create(user=self.user,name="Cilantro")
+        payload={"name":"Coriander"}
+        url=detail_url(ingredient_id=ingredient.id)
+        res=self.client.patch(url,payload)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
