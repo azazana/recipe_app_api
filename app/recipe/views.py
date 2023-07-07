0b2cb5b5ba8dac207ec404296df012623e639d5a
azazana <azazana@list.ru>
2023-07-07 22:02:39 +0300

    flake fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -66,7 +66,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
-        return queryset.filter(user=self.request.user).order_by("-id").distinct()
+        return queryset.filter(user=self.request.user).\
+            order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
@@ -92,11 +93,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @extend_schema_view(
     list=extend_schema(
         parameters=[OpenApiParameter(
             "assigned_only",
-            OpenApiTypes.INT,enum=[0,1],
+            OpenApiTypes.INT, enum=[0, 1],
             description="Filter by items assigned to recipes."
         )]
     )
@@ -118,7 +120,8 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user).order_by("-name").distinct()
+        return queryset.filter(user=self.request.user).\
+            order_by("-name").distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
